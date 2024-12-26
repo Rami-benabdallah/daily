cb17b0b210f1724ed5b0e52d53167da937ae30c8
rami <rami.benhadjabdallah@vocanto.de>
2024-12-26 20:02:07 +0100

    update gitignore

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("43bcc964924d8c0f")
+/******/ 		__webpack_require__.h = () => ("65467c059bbd35f7")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
